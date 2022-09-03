3e7352c817683328e4da190abfab48683821d5a3
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-03 22:04:46 +0200

    47. bzowa nawigacja mobilna

--- frontent/src/shared/components/Navigation/MainNavigation.js
+++ frontent/src/shared/components/Navigation/MainNavigation.js
@@ -2,25 +2,33 @@ import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        {/* trzy puste spany do hamburgera */}
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-        {/* navLink później */}
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          {/* trzy puste spany do hamburgera */}
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          {/* navLink później */}
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
