cb42692931e4e586951d324d515a68ca425db291
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-04 21:49:33 +0200

    52 const { userId }= useParams(); react-router-dom

--- frontent/src/places/pages/UserPlaces.jsx
+++ frontent/src/places/pages/UserPlaces.jsx
@@ -1,3 +1,5 @@
+import { useParams } from "react-router-dom"; //ten funkcyjny hook działa tylko w funkcyjncyh komponentach
+
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -30,7 +32,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const { userId } = useParams();
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
