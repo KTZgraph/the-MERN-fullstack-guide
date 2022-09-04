70e2f39565e48dff583e425578edd53895a11169
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-04 18:56:23 +0200

    48 SideDrawer Portal

--- frontent/src/shared/components/Navigation/SideDrawer.js
+++ frontent/src/shared/components/Navigation/SideDrawer.js
@@ -1,6 +1,16 @@
+import { createPortal } from "react-dom";
 import "./SideDrawer.css";
+
+// WARNING React Portal szczególnie interesujace gdy zagnieżdżone w mocno zagnieżdżonych komponentach, bo pozwala na wyrenderowanie elementu w całkiem innym miejscu niż komponent w którym się zanjduje
+// tutaj SideDrawer znajduje się w the-MERN-fullstack-guide\frontent\src\shared\components\Navigation\MainNavigation.js
+// komponent jest elementem drzewa DOM/drzewa komponentów, ale kontent jest wyrenderowany gdzie indziej - to jest portal
+
 const SideDrawer = (props) => {
-  return <aside className="side-drawer">{props.children}</aside>;
+  // można też zapisywać JSX do zmiennej, nie tylko go zwracać
+  const content = <aside className="side-drawer">{props.children}</aside>;
+
+  // https://stackoverflow.com/questions/69974866/why-reactdom-createportal-does-not-work-in-my-content-script
+  return createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawer;
